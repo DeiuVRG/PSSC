c149c0dfd531dcda91617d679678098b2e6d4025
deiuvrg <rusuandrei30.11@gmail.com>
2025-10-02 09:12:42 +0300

    VEDEM CE DOAMNE IESE

---
+++ L1/Lab1/Lab1/Domain/Product.cs
@@ -0,0 +1,4 @@
+namespace Lab1.Domain;
+
+// Pretul este per unitate sau per kg
+public record Product(string Name, decimal PricePerUnit);
---
+++ L1/Lab1/Lab1/Domain/iQuantity.cs
@@ -0,0 +1,8 @@
+namespace Lab1.Domain;
+
+// Interface simpla pentru cantitate.
+// Daca ajungi sa pui aici AI si machine learning, inseamna ca ai luat-o prea personal.
+public interface IQuantity
+{
+    decimal Amount { get; }
+}
